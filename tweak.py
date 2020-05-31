876c62a0290fe9cf4f54bd01f09a5f3698273336
sourabhskc <64469149+sourabhskc@users.noreply.github.com>
2020-06-01 00:48:22 +0530

    Update tweak.py

--- tweak.py
+++ tweak.py
@@ -1,8 +1,8 @@
 import os
 accuracy = os.system("cat /taskCode/accuracy.txt")
 x = 'model.add(Dense(units=32, activation=\"relu\"))'
-if accuracy < 98:
+if accuracy < 97:
     os.system("sed -i '/softmax/ i {}' /taskCode/train.py".format(x))
 else:
-    print("Good going :)")
+    print("Accuracy achieved !!!")
     exit()
