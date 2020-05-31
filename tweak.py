3ddbfbb6774c97adbfe1b42a4f67483cf18de4b6
sourabhskc <64469149+sourabhskc@users.noreply.github.com>
2020-05-31 23:20:31 +0530

    accuracy changed to 97%

--- tweak.py
+++ tweak.py
@@ -1,7 +1,7 @@
 import os
 accuracy = os.system("cat /taskCode/accuracy.txt")
 x = 'model.add(Dense(units=32, activation=\"relu\"))'
-if accuracy < 96:
+if accuracy < 97:
     os.system("sed -i '/softmax/ i {}' /taskCode/model_code.py".format(x))
 else:
     print("Good going :)")
